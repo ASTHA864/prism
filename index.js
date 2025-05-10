c8a49af0d9bf570768d3a056f41c6ec15d0b466e
blanky <aaishwarymishra@gmail.com>
2025-05-10 13:00:42 +0530

    added the gemini api end-point

---
+++ index.js
@@ -0,0 +1,51 @@
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import { GoogleGenAI } from "@google/genai";
+import fs from "fs";
+import { types } from "util";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const prompt = fs.readFileSync("prompt.txt", "utf-8");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.get("/generate_output", async (req, res) => {
+  try {
+    const inputText = req.query.input_text;
+    if (!inputText) {
+      return res.status(400).send("Input text is required");
+    }
+    const ai = new GoogleGenAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    });
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: [
+        {
+          type: "text",
+          text: inputText,
+        },
+      ],
+      config: {
+        systemInstruction: prompt,
+      },
+    });
+    res.json({ content: response.text });
+  } catch (error) {
+    console.error("Error generating output:", error);
+    res.status(500).send("Error generating output");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
